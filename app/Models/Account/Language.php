17a129f0ce99015a3211bd621ddbe95b63c95b5d
Alexander <shapoval.webdev@gmail.com>
2020-08-04 17:45:51 +0300

    Добавлены связи между моделями

--- app/Models/Account/Language.php
+++ app/Models/Account/Language.php
@@ -2,11 +2,35 @@
 
 namespace App\Models\Account;
 
+use App\Models\Geo\CityTranslate;
+use App\Models\Geo\CountryTranslate;
+use App\Models\Geo\StateTranslate;
+use App\Models\Marketing\VendorTranslate;
 use App\Models\Traits\UuidTrait;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class Language
+ *
+ * @package App\Models\Account
+ * @property string                       $id
+ * @property string                       $name
+ * @property string                       $code
+ * @property boolean                      $is_primary
+ * @property Carbon                       $created_at
+ * @property Carbon                       $updated_at
+ * @property Collection<CountryTranslate> $countryTranslates
+ * @property Collection<StateTranslate>   $stateTranslates
+ * @property Collection<CityTranslate>    $cityTranslates
+ * @property Collection<VendorTranslate>  $vendorTranslates
+ */
 class Language extends Model
 {
+    use UuidTrait;
+
     const LANGUAGE_EN = 'en';
     const LANGUAGE_ES = 'es';
     const LANGUAGE_RU = 'ru';
@@ -27,7 +51,47 @@ class Language extends Model
         self::LANGUAGE_UK,
     ];
 
-    use UuidTrait;
-
     protected $table = 'account_languages';
+
+    protected $fillable = [
+        'name',
+        'code',
+        'is_primary',
+    ];
+
+    /*
+     * Relations
+     */
+
+    /**
+     * @return HasMany
+     */
+    public function countryTranslates(): HasMany
+    {
+        return $this->hasMany(CountryTranslate::class, 'language_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function stateTranslates(): HasMany
+    {
+        return $this->hasMany(StateTranslate::class, 'language_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function cityTranslates(): HasMany
+    {
+        return $this->hasMany(CityTranslate::class, 'language_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function vendorTranslates(): HasMany
+    {
+        return $this->hasMany(VendorTranslate::class, 'language_id');
+    }
 }
--- app/Models/Account/SocialAccount.php
+++ app/Models/Account/SocialAccount.php
@@ -2,9 +2,31 @@
 
 namespace App\Models\Account;
 
+use App\Models\Marketing\Company;
 use App\Models\Traits\UuidTrait;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class SocialAccount
+ *
+ * @package App\Models\Account
+ * @property string              $id
+ * @property string              $user_id
+ * @property string              $provider_id
+ * @property string              $provider_name
+ * @property string              $email
+ * @property string              $access_token
+ * @property string              $refresh_token
+ * @property Carbon              $created_at
+ * @property Carbon              $updated_at
+ * @property Carbon              $last_login_at
+ * @property User                $user
+ * @property Collection<Company> $companies
+ */
 class SocialAccount extends Model
 {
     use UuidTrait;
@@ -12,11 +34,12 @@ class SocialAccount extends Model
     protected $table = 'account_socials';
 
     protected $fillable = [
-        'uuid',
         'user_id',
         'provider_id',
         'provider_name',
         'email',
+        'access_token',
+        'refresh_token',
         'last_login_at',
     ];
 
@@ -25,10 +48,18 @@ class SocialAccount extends Model
      */
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @return BelongsTo
      */
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function companies(): HasMany
+    {
+        return $this->hasMany(Company::class, 'social_account_id');
     }
 }
