6fde220799a9a0dcb7b8269e2bf0312f319bc446
Prosspa <prosspas@gmail.com>
2021-05-24 11:39:52 +0300

    delete migration

--- app/Models/Marketing/Project.php
+++ app/Models/Marketing/Project.php
@@ -123,11 +123,4 @@ class Project extends Model
         return $this->hasMany(Campaign::class, 'project_id');
     }
 
-    /**
-     * @return HasMany
-     */
-    public function platforms(): HasMany
-    {
-        return $this->hasMany(Platforms::class, 'project_id');
-    }
 }
